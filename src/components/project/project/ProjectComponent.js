23b9960c168e9fd4d9118222245d1a8a742bad52
minsun21 <mspark0429@daum.net>
2021-03-22 22:42:40 +0900

    프로젝트 부분 시작

--- src/components/project/project/ProjectComponent.js
+++ src/components/project/project/ProjectComponent.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { DataGrid } from '@material-ui/data-grid';
 import Button from '@material-ui/core/Button';
 
-const CompanyCompo = styled.div`
+const ProjectCompo = styled.div`
     background-color:#fff;
     width:100%;
     padding: 20px;
@@ -18,10 +18,13 @@ const buttonStyle = {
     marginBottom: "10px",
 }
 
-export default class CompanyComponent extends Component {
+export default class ProjectComponent extends Component {
     render() {
         const columns = [
-            { field: 'companyName', headerName: '이름', width: 200 },
+            { field: 'title', headerName: '이름', width: 200 },
+            { field: 'desc', headerName: '내용', width: 200 },
+            { field: 'startDate', headerName: '시작일', width: 200 },
+            { field: 'endDate', headerName: '종료일', width: 200 },
             {
                 field: 'modify',
                 headerName: '수정',
@@ -32,7 +35,7 @@ export default class CompanyComponent extends Component {
                         color="primary"
                         size="small"
                         style={{ marginLeft: 16 }}
-                        onClick={() => this.props.modifyCompany(params.row)}
+                        onClick={() => this.props.modifyProject(params.row)}
                     >
                         수정
                     </Button>
@@ -48,7 +51,7 @@ export default class CompanyComponent extends Component {
                         color="primary"
                         size="small"
                         style={{ marginLeft: 16 }}
-                        onClick={() => this.props.deleteCompany(params.row.id)}
+                        onClick={() => this.props.deleteProject(params.row.id)}
                     >
                         삭제
                     </Button>
@@ -56,14 +59,14 @@ export default class CompanyComponent extends Component {
             },
         ];
         return (
-            <CompanyCompo>
-                <Button variant="outlined" color="primary" onClick={this.props.createCompany} style={buttonStyle}>
+            <ProjectCompo>
+                <Button variant="outlined" color="primary" onClick={this.props.createProject} style={buttonStyle}>
                     등록
                 </Button>
                 <TableCompo>
-                    <DataGrid rows={this.props.companyList} columns={columns} disableExtendRowFullWidth hideFooterSelectedRowCount={true} />
+                    <DataGrid rows={this.props.projectList} columns={columns} disableExtendRowFullWidth hideFooterSelectedRowCount={true} />
                 </TableCompo>
-            </CompanyCompo>
+            </ProjectCompo>
         )
     }
 }
--- src/components/project/project/ProjectManagement.js
+++ src/components/project/project/ProjectManagement.js
@@ -1,12 +1,88 @@
 import React, { Component } from 'react'
-import styled from "styled-components";
+import GetFunc from '../../../api/GetFunc';
+import DialogModal from '../modal/DialogModal';
+import ProjectComponent from './ProjectComponent';
+import { CREATE, MODIFY, DELETE, SUCCESS } from "../../../common/Constants";
+import CreateProject from '../modal/CreateProject';
 
 export default class ProjectManagement extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projectList: [],
+            open: false,
+            modalType: "",
+            modalTitle: "",
+            companyName: "",
+            select: "",
+        }
+        this.close = this.close.bind(this);
+        this.handleOk = this.handleOk.bind(this);
+        this.onHandleChange = this.onHandleChange.bind(this);
+        // this.createCompany = this.createCompany.bind(this);
+        // this.create = this.create.bind(this);
+        // this.renderModal = this.renderModal.bind(this);
+        // this.modifyCompany = this.modifyCompany.bind(this);
+        // this.resultAction = this.resultAction.bind(this);
+        // this.deleteCompany = this.deleteCompany.bind(this);
+    }
+
+    componentDidMount() {
+        GetFunc.allProject().then(res => {
+            if (res.data.result) {
+                this.setState({ projectList: res.data.data })
+            } else {
+                alert(res.data.result);
+            }
+        })
+    }
+
+    onHandleChange(e) {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    close() {
+        this.setState({ open: false, modalType: "", modalTitle: "" })
+    }
+
+    handleOk() {
+        let type = this.state.modalType;
+        if (type === CREATE) {
+            this.create();
+        } else if (type === MODIFY) {
+            this.modify();
+        } else if (type === DELETE) {
+            this.delete();
+        }
+    }
+
+    renderModal() {
+        let type = this.state.modalType;
+        if (type === CREATE) {
+            return <CreateProject onHandleChange={this.onHandleChange} />
+        } else if (type === MODIFY) {
+            return <CreateProject onHandleChange={this.onHandleChange} prevInfo={this.state.companyName} />
+        } else if (type === DELETE) {
+            return <p> 삭제 하시겠습니까? </p>
+        }
+    }
+
+    resultAction(res) {
+        if (res.data.result === SUCCESS) {
+            this.setState({ projectList: res.data.data, open: false, modalType: "", select: "" });
+        } else {
+            alert(res.data.result);
+        }
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                <DialogModal open={this.state.open} close={this.close} modalTitle={this.state.modalTitle} handleOk={this.handleOk}>
+                    {this.renderModal()}
+                </DialogModal>
+                <ProjectComponent projectList={this.state.projectList} createProejct={this.createProejct} modifyProject={this.modifyProject} deleteProject={this.deleteProject} />
+            </>
         )
     }
 }
